dc2f1c6e160b077cfe0ed485f5b07d373e368cb8
Tetsuo Kiso <tetsuo-s@is.naist.jp>
2012-03-25 15:22:30 +0900

    Clean up ATables.

--- GIZA++-v2/ATables.cpp
+++ GIZA++-v2/ATables.cpp
@@ -8,8 +8,6 @@
   as published by the Free Software Foundation; either version 2
   of the License, or (at your option) any later version.
 
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -26,81 +24,55 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #include "myassert.h"
 #include "Parameter.h"
 
-GLOBAL_PARAMETER(bool,CompactADTable,"compactadtable","1: only 3-dimensional alignment table for IBM-2 and IBM-3",PARLEV_MODELS,1);
-GLOBAL_PARAMETER(float,amodel_smooth_factor,"model23SmoothFactor","smoothing parameter for IBM-2/3 (interpolation with constant)",PARLEV_SMOOTH,0.0);
+GLOBAL_PARAMETER(bool, CompactADTable, "compactadtable", "1: only 3-dimensional alignment table for IBM-2 and IBM-3", PARLEV_MODELS, 1);
+GLOBAL_PARAMETER(float, amodel_smooth_factor, "model23SmoothFactor", "smoothing parameter for IBM-2/3 (interpolation with constant)", PARLEV_SMOOTH, 0.0);
 
 template <class VALTYPE>
-void amodel<VALTYPE>::printTable(const char *filename) const
-  // print amodel to file with the name filename (it'll be created or overwritten 
-  // format : for a table : 
-  //              aj j l m  val 
-  // where aj is source word pos, j target word pos, l source sentence length, 
-  //       m is target sentence length.
-  //          
-{
-  //return;
+void amodel<VALTYPE>::printTable(const char *filename) const {
   if (is_distortion)
     cout << "Dumping distortion table (d) to file:" << filename <<'\n';
-  else 
+  else
     cout << "Dumping alignment table (a) to file:" << filename <<'\n';
 
   ofstream of(filename);
-  double ssum=0.0;
-  for(WordIndex l=0; l < MaxSentLength; l++)
-    for(WordIndex m=0;m<MaxSentLength;m++)
-      {
-	if( CompactADTable && l!=m )
-	  continue;
-	unsigned int L=((CompactADTable&&is_distortion)?MaxSentLength:(l+1))-1;
-	unsigned int M=((CompactADTable&&!is_distortion)?MaxSentLength:(m+1))-1;
-	if( is_distortion==0 )
-	  for(WordIndex j=1;j<=M; j++)
-	    {
-	      double sum=0.0;
-	      for(WordIndex i=0;i<=L; i++)
-		{
-		  VALTYPE x=getValue(i, j, L, M); 
-		  if( x>PROB_SMOOTH )
-		    {
-		      of << i << ' ' << j << ' ' << L << ' ' << M << ' ' << x << '\n';
-		      sum+=x;
-		    }
-		}
-	      ssum+=sum;
-	    }
-	else
-	  for(WordIndex i=0;i<=L;i++)
-	    {
-	      double sum=0.0;
-	      for(WordIndex j=1;j<=M;j++)
-		
-		{
-		  VALTYPE x=getValue(j, i, L, M);
-		  if( x>PROB_SMOOTH )
-		    {
-		      of << j << ' ' << i << ' ' << L << ' ' << M << ' ' << x << '\n';
-		      sum+=x;
-		    }
-		}
-	      ssum+=sum;
-	    }
+  double ssum = 0.0;
+  for (WordIndex l = 0; l < MaxSentLength; l++) {
+    for (WordIndex m = 0; m < MaxSentLength; m++) {
+      if (CompactADTable && l != m)
+        continue;
+      unsigned int L = ((CompactADTable&&is_distortion)?MaxSentLength:(l+1))-1;
+      unsigned int M = ((CompactADTable&&!is_distortion)?MaxSentLength:(m+1))-1;
+      if(is_distortion == 0) {
+        for (WordIndex j = 1;j <= M; j++) {
+          double sum=0.0;
+          for (WordIndex i = 0; i <= L; i++) {
+            VALTYPE x=getValue(i, j, L, M);
+            if (x > PROB_SMOOTH) {
+              of << i << ' ' << j << ' ' << L << ' ' << M << ' ' << x << '\n';
+              sum+=x;
+            }
+          }
+          ssum+=sum;
+        }
+      } else {
+        for (WordIndex i = 0; i <= L; i++) {
+          double sum = 0.0;
+          for (WordIndex j = 1; j <= M; j++) {
+            VALTYPE x=getValue(j, i, L, M);
+            if (x > PROB_SMOOTH) {
+              of << j << ' ' << i << ' ' << L << ' ' << M << ' ' << x << '\n';
+              sum+=x;
+            }
+          }
+          ssum+=sum;
+        }
       }
+    }
+  }
 }
 
 template <class VALTYPE>
-void amodel<VALTYPE>::readTable(const char *filename)
-{
-  /* This function reads the a table from a file.
-     Each line is of the format:  aj j l m  val
-     where aj is the source word position, j the target word position, 
-     l the source sentence length, and m the target sentence length
-     
-     This function also works for a d table, where the positions
-     of aj and i are swapped.  Both the a and d tables are 4 dimensional
-     hashes; this function will simply read in the four values and keep
-     them in that order when hashing the fifth value.
-     NAS, 7/11/99
-  */
+void amodel<VALTYPE>::readTable(const char *filename) {
   ifstream inf(filename);
   cout << "Reading a/d table from " << filename << "\n";
   if(!inf){
@@ -109,10 +81,11 @@ void amodel<VALTYPE>::readTable(const char *filename)
   }
   WordIndex w, x, l, m;
   VALTYPE prob;
-  while(inf >> w >> x >> l >> m >> prob )
+  while(inf >> w >> x >> l >> m >> prob ) {
     // the NULL word is added to the length
-    // of the sentence in the tables, but discount it when you write the tables. 
+    // of the sentence in the tables, but discount it when you write the tables.
     setValue(w, x, l, m, prob);
+  }
 }
 
 template class amodel<COUNT>;
--- GIZA++-v2/ATables.h
+++ GIZA++-v2/ATables.h
@@ -8,8 +8,6 @@
   as published by the Free Software Foundation; either version 2
   of the License, or (at your option) any later version.
 
-
-USA.
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -59,34 +57,38 @@ extern short NoEmptyWord;
    table is the probability (d(j/l,m,i), where j is word target position, i is
    source word position connected to it, m is target sentence length, and l is
    source sentence length) or count collected for it. The  probability and the
-   count are represented as log integer probability as 
-   defined by the class LogProb .  
+   count are represented as log integer probability as
+   defined by the class LogProb .
 
   This class is used to represents a Tables (probabiliity) and d (distortion)
   tables and also their corresponding count tables .
-  
+
   *--------------------------------------------------------------------------*/
 
-
 template <class VALTYPE>
-class amodel
-{
+class amodel {
  public:
   Array4<VALTYPE> a;
-  bool is_distortion ; 
+  bool is_distortion ;
   WordIndex MaxSentLength;
   bool ignoreL, ignoreM;
-  VALTYPE get(WordIndex aj, WordIndex j, WordIndex l, WordIndex m)const
-    {
-      massert( (!is_distortion) || aj<=m );massert( (!is_distortion) || j<=l );massert( (!is_distortion) || aj!=0 );
-      massert( is_distortion    || aj<=l );massert( is_distortion    || j<=m );massert( (is_distortion) || j!=0 );
-      massert( l<MaxSentLength );massert( m<MaxSentLength );
-      return a.get(aj, j, (CompactADTable&&is_distortion)?MaxSentLength:(l+1),(CompactADTable&&!is_distortion)?MaxSentLength:(m+1));
-    }
+
   static float smooth_factor;
-  amodel(bool flag)
-    : a(MAX_SENTENCE_LENGTH+1,0.0), is_distortion(flag), MaxSentLength(MAX_SENTENCE_LENGTH)
-    {}; 
+
+  explicit amodel(bool flag)
+      : a(MAX_SENTENCE_LENGTH + 1, 0.0),
+        is_distortion(flag),
+        MaxSentLength(MAX_SENTENCE_LENGTH) { }
+
+  ~amodel() {}
+
+  VALTYPE get(WordIndex aj, WordIndex j, WordIndex l, WordIndex m) const {
+    massert( (!is_distortion) || aj<=m );massert( (!is_distortion) || j<=l );massert( (!is_distortion) || aj!=0 );
+    massert( is_distortion    || aj<=l );massert( is_distortion    || j<=m );massert( (is_distortion) || j!=0 );
+    massert( l<MaxSentLength );massert( m<MaxSentLength );
+    return a.get(aj, j, (CompactADTable&&is_distortion)?MaxSentLength:(l+1),(CompactADTable&&!is_distortion)?MaxSentLength:(m+1));
+  }
+
   VALTYPE&getRef(WordIndex aj, WordIndex j, WordIndex l, WordIndex m)
     {
       massert( (!is_distortion) || aj<=m );massert( (!is_distortion) || j<=l );
@@ -103,65 +105,78 @@ class amodel
       if( is_distortion==0 )
 	return max(double(PROB_SMOOTH),amodel_smooth_factor/(l+1)+(1.0-amodel_smooth_factor)*get(aj, j, l, m));
       else
-	return max(double(PROB_SMOOTH),amodel_smooth_factor/m+(1.0-amodel_smooth_factor)*get(aj, j, l, m));	
-    }
-  void printTable(const char* filename)const ; 
-  template<class COUNT>
-  void normalize(amodel<COUNT>& aTable)const
-    {
-      WordIndex i, j, l, m ;
-      COUNT total;
-      int nParam=0;
-      for(l=0;l<MaxSentLength;l++)
-	for(m=0;m<MaxSentLength;m++)
-	  {
-	    if( CompactADTable && l!=m )
-	      continue;
-	    unsigned int L=((CompactADTable&&is_distortion)?MaxSentLength:(l+1))-1;
-	    unsigned int M=((CompactADTable&&!is_distortion)?MaxSentLength:(m+1))-1;
-	    if( is_distortion==0 )
-	      for(j=1;j<=M; j++)
-		{
-		  total=0.0;
-		  for(i=0;i<=L;i++)
-		    {
-		      total+=get(i, j, L, M);
-		    }
-		  if( total )
-		    for(i=0;i<=L;i++)
-		      {
-			nParam++;
-			aTable.getRef(i, j, L, M)=get(i, j, L, M)/total;
-			massert(aTable.getRef(i,j,L,M)<=1.0);
-			if( NoEmptyWord&&i==0 )
-			  aTable.getRef(i,j,L,M)=0;
-		      }
-		}
-	    else
-	      for(i=0;i<=L;i++)
-		{
-		  total=0.0;
-		  for(j=1;j<=M;j++)
-		    total+=get(j, i, L, M);
-		  if( total )
-		    for(j=1;j<=M;j++)
-		      {
-			aTable.getRef(j, i, L, M)=amodel_smooth_factor/M+(1.0-amodel_smooth_factor)*get(j, i, L, M)/total;
-			nParam++;
-			massert(aTable.getRef(j,i,L,M)<=1.0);
-			if( NoEmptyWord&&i==0 )
-			  aTable.getRef(j,i,L,M)=0;
-		      }
-		}
-	  }
-      cout << "A/D table contains " << nParam << " parameters.\n";
+	return max(double(PROB_SMOOTH),amodel_smooth_factor/m+(1.0-amodel_smooth_factor)*get(aj, j, l, m));
     }
 
-  void readTable(const char *filename); 
-  void clear()
-    {a.clear();}
+  // Print amodel to file with the name filename (it'll be created or overwritten
+  // format : for a table :
+  //              aj j l m  val
+  // where aj is source word pos, j target word pos, l source sentence length,
+  //       m is target sentence length.
+  //
+  void printTable(const char* filename) const;
+
+  template <class COUNT>
+  void normalize(amodel<COUNT>& aTable) const {
+    WordIndex i, j, l, m ;
+    COUNT total;
+    int nParam = 0;
+    for (l = 0;l < MaxSentLength; l++) {
+      for (m = 0; m < MaxSentLength; m++) {
+        if (CompactADTable && l!=m)
+          continue;
+        unsigned int L = ((CompactADTable&&is_distortion)?MaxSentLength:(l+1))-1;
+        unsigned int M = ((CompactADTable&&!is_distortion)?MaxSentLength:(m+1))-1;
+        if (is_distortion == 0) {
+          for (j = 1; j <= M; j++) {
+            total = 0.0;
+            for (i = 0; i <= L; i++) {
+              total += get(i, j, L, M);
+            }
+            if (total ) {
+              for (i=0;i<=L;i++) {
+                nParam++;
+                aTable.getRef(i, j, L, M)=get(i, j, L, M)/total;
+                massert(aTable.getRef(i,j,L,M)<=1.0);
+                if( NoEmptyWord&&i==0 )
+                  aTable.getRef(i,j,L,M)=0;
+              }
+            }
+          }
+        } else {
+          for (i=0;i<=L;i++) {
+            total=0.0;
+            for (j=1;j<=M;j++) {
+              total += get(j, i, L, M);
+            }
+            if (total) {
+              for (j = 1; j <= M;j++) {
+                aTable.getRef(j, i, L, M) = amodel_smooth_factor / M + (1.0-amodel_smooth_factor) * get(j, i, L, M) / total;
+                nParam++;
+                massert(aTable.getRef(j,i,L,M)<=1.0);
+                if( NoEmptyWord&&i==0 )
+                  aTable.getRef(j,i,L,M)=0;
+              }
+            }
+          }
+        }
+      }
+    }
+    cout << "A/D table contains " << nParam << " parameters.\n";
+  }
+
+  // Reads the a table from a file.
+  // Each line is of the format:  aj j l m  val
+  // where aj is the source word position, j the target word position,
+  // l the source sentence length, and m the target sentence length
+  //
+  // This function also works for a d table, where the positions
+  // of aj and i are swapped.  Both the a and d tables are 4 dimensional
+  // hashes; this function will simply read in the four values and keep
+  // them in that order when hashing the fifth value.
+  // NAS, 7/11/99
+  void readTable(const char *filename);
+  void clear() { a.clear(); }
 };
-
-/* ------------------- End of amodel Class Definitions ----------------------*/
 
 #endif
