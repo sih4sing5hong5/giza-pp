1fe24540435681c7448f2a5e7ac090dfb27fe221
Tetsuo Kiso <tetsuo-s@is.naist.jp>
2012-03-25 18:08:07 +0900

    Clean up snt2cooc.

--- GIZA++-v2/snt2cooc.cpp
+++ GIZA++-v2/snt2cooc.cpp
@@ -9,99 +9,135 @@
 
 using namespace std;
 
-void readVoc(istream&in,map<string,string>&voc)
-{
-  string line,s1,s2; 
-  voc["1"]="UNK";
-  if( !in )cerr <<"Vocabulary does not exist.\n";
-  while(getline(in,line))
-    {
-      istringstream eingabe(line); 
-      if( !(eingabe>>s1>>s2))
-	cerr << "ERROR in vocabulary '" << line << "'\n";
-      voc[s1]=s2;
+bool ReadVocab(const char* filename, map<string, string>& voc) {
+  ifstream ifs(filename);
+  if (!ifs) {
+    cerr << "Vocabulary does not exist.\n";
+    return false;
+  }
+
+  voc.clear();
+
+  string line, s1, s2;
+  voc["1"] = "UNK";
+
+  while (getline(ifs, line)) {
+    istringstream eingabe(line);
+    if (!(eingabe >> s1 >> s2)) {
+      cerr << "ERROR in vocabulary '" << line << "'\n";
+      return false;
     }
+    voc[s1] = s2;
+  }
+  return true;
 }
 
-int maxElems=0;
-int main(int argc,char **argv)
-{
-  if( argc!=4&&argc!=5 )
-    {
-      cerr << "Usage: " << argv[0] << " vcb1 vcb2 snt12 \n";
-      cerr << "Converts GIZA++ snt-format into plain text.\n";
-      exit(1);
+
+int main(int argc, char **argv) {
+  if (argc != 4 && argc != 5) {
+    cerr << "Usage: " << argv[0] << " vcb1 vcb2 snt12 \n";
+    cerr << "Converts GIZA++ snt-format into plain text.\n";
+    exit(1);
+  }
+
+  int max_elements = 0;
+  bool counts = false;
+
+  if (argc == 5) {
+    if (string(argv[4]) != "-counts") {
+      cerr << "ERROR: wrong option " << argv[5] << endl;
     }
-  bool counts=0;
-  if( argc==5 )
-    {
-      if(string(argv[4])!="-counts")
-	cerr << "ERROR: wrong option " << argv[5] << endl;
-      counts=1;
-	maxElems=10000000;
+    counts = true;
+    max_elements = 10000000;
+  }
+
+  ifstream t(argv[3]);
+  if (!t) {
+    cerr << "No such file or directory: " << argv[3] << endl;
+    exit(1);
+  }
+
+  map<string, string> voc1, voc2;
+
+  if (!ReadVocab(argv[1], voc1)) {
+    cerr << "Failed to read vocabulary." << endl;
+    exit(1);
+  }
+
+  if (!ReadVocab(argv[2], voc2)) {
+    cerr << "Failed to read vocabulary." << endl;
+    exit(1);
+  }
+
+  string line1, line2, line3;
+  vector<map<int,int> > vsi(voc1.size() + 1000);
+
+  int line_num = 0;
+  int total_elements = 0;
+  while (getline(t, line1) && getline(t, line2) && getline(t, line3)) {
+    istringstream eingabe1(line1), eingabe2(line2), eingabe3(line3);
+    double count;
+    string word;
+
+    eingabe1 >> count;
+    vector<int>l1,l2;
+
+    while(eingabe2 >> word) {
+      l1.push_back(atoi(word.c_str()));
     }
-  ifstream v1(argv[1]),v2(argv[2]),t(argv[3]);
-  map<string,string>voc1,voc2;
-  readVoc(v1,voc1);
-  readVoc(v2,voc2);
-  string line1,line2,line3;
-  vector<map<int,int> > vsi(voc1.size()+1000);
-  int nLine=0;
-  int totalElems=0;
-  while(getline(t,line1)&&getline(t,line2)&&getline(t,line3))
-    {
-      istringstream eingabe1(line1),eingabe2(line2),eingabe3(line3);
-      double count;
-      string word;
-      eingabe1>>count;
-      vector<int>l1,l2;
-      while(eingabe2>>word)
-	l1.push_back(atoi(word.c_str()));
-      while(eingabe3>>word)
-	l2.push_back(atoi(word.c_str()));
-      if( ((++nLine)%1000)==0 )
-	cerr << "line " << nLine << '\n'; 
-      totalElems-=vsi[0].size();
-      for(unsigned int j=0;j<l2.size();++j)
-	vsi[0][l2[j]]++;
-      totalElems+=vsi[0].size();
-      for(unsigned int i=0;i<l1.size();++i)
-	{
-	  if( l1[i]>=int(vsi.size()) )
-	    {
-	      cerr << "I have to resize: " << l1[i] << endl;
-	      vsi.resize(l1[i]+1);
-	    }
-	  map<int,int>&theset=vsi[l1[i]];
-	  totalElems-=theset.size();
-	  for(unsigned int j=0;j<l2.size();++j)
-	    theset[l2[j]]++;
-	  totalElems+=theset.size();
-	}
-      if( totalElems>maxElems&&maxElems )
-	{
-	  cerr << "INFO: print out " << totalElems << " entries.\n";
-	  for(unsigned int i=0;i<vsi.size();++i)
-	    for(map<int,int>::const_iterator j=vsi[i].begin();j!=vsi[i].end();++j)
-	      {
-		if(counts==1 )
-		  cout << j->second << " " << i << " " << j->first << '\n';
-		else
-		  cout << i << " " << j->first << '\n';
-	      }
-	  totalElems=0;
-	  vsi.clear();
-	  vsi.resize(voc1.size()+1000);
-	}
+    while(eingabe3 >> word) {
+      l2.push_back(atoi(word.c_str()));
     }
-  cerr << "END.\n";
-  for(unsigned int i=0;i<vsi.size();++i)
-    for(map<int,int>::const_iterator j=vsi[i].begin();j!=vsi[i].end();++j)
-      {
-	if(counts==1 )
-	  cout << j->second << " " << i << " " << j->first << '\n';
-	else
-	  cout << i << " " << j->first << '\n';
+
+    if (((++line_num) % 1000) == 0) {
+      cerr << "line " << line_num << '\n';
+    }
+
+    total_elements -= vsi[0].size();
+    for(unsigned int j=0;j<l2.size();++j) {
+      vsi[0][l2[j]]++;
+    }
+    total_elements += vsi[0].size();
+
+    for (unsigned int i = 0; i < l1.size(); ++i) {
+      if (l1[i]>=int(vsi.size())) {
+        cerr << "I have to resize: " << l1[i] << endl;
+        vsi.resize(l1[i]+1);
       }
-}
+      map<int,int>& theset = vsi[l1[i]];
+      total_elements -= theset.size();
+      for (unsigned int j = 0; j < l2.size(); ++j) {
+        theset[l2[j]]++;
+      }
+      total_elements += theset.size();
+    }
+    if (total_elements > max_elements && max_elements) {
+      cerr << "INFO: print out " << total_elements << " entries.\n";
+      for (unsigned int i=0;i<vsi.size();++i) {
+        for (map<int,int>::const_iterator j=vsi[i].begin();j!=vsi[i].end();++j) {
+          if (counts) {
+            cout << j->second << " " << i << " " << j->first << '\n';
+          } else {
+            cout << i << " " << j->first << '\n';
+          }
+        }
+      }
+      total_elements = 0;
+      vsi.clear();
+      vsi.resize(voc1.size()+1000);
+    }
+  }
+  cerr << "END.\n";
+  for (unsigned int i = 0; i < vsi.size(); ++i) {
+    for (map<int,int>::const_iterator j= vsi[i].begin();
+         j != vsi[i].end(); ++j) {
+      if (counts) {
+        cout << j->second << " " << i << " " << j->first << '\n';
+      } else {
+        cout << i << " " << j->first << '\n';
+      }
+    }
+  }
 
+  return 0;
+}
